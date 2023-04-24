84b82e00029c616e5cd2e7333fd1dc4788c2ecaf
LuizGustavoSalvador <luiz.gustavo@nexello.com.br>
2023-04-24 14:56:02 -0300

    ajustes btn questions action

---
+++ assets/js/question.js
@@ -0,0 +1,130 @@
+let lastId = 0;
+let questionConfig = [];
+let urlDefault = 'http://localhost:3000/test/question';
+const maxOptions = getCookie("maxOptions");
+
+function getCookie(cookieName) {
+  let cookie = {};
+
+  document.cookie.split(';').forEach(function(el) {
+    let [key,value] = el.split('=');
+    cookie[key.trim()] = value;
+  })
+
+  return cookie[cookieName];
+}
+
+function submit() {
+  const result = {
+    test_id: document.querySelector("form #testId").value,
+    questions: []
+  };
+
+  for (const key in questionConfig) {
+    if (Object.hasOwnProperty.call(questionConfig, key)) {
+      const row = questionConfig[key];
+      result.questions.push({
+        question: document.getElementById(row.question).value,
+        answer: document.getElementById(row.answer).value,
+        options: row.options.map((o, i) => ({ 
+          text: document.getElementById(o.input).value,
+          value: o.value,
+        })),
+      });
+    }
+  }
+
+  fetch('http://localhost:3000/test/question/store', {
+    method: 'POST',
+    headers: {
+        //'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(result)
+  })
+};
+
+function createQuestion() {
+  lastId++;
+
+  if (lastId > getCookie('maxQuestions')) {
+    let button = document.getElementById('addQuestion');
+    button.disabled = true;
+    button.classList.add('disabled-button');
+    button.setAttribute('title', 'você só pode adicionar ' +getCookie("maxQuestions")+ ' questões neste teste');
+
+    return;
+  }
+
+  questionConfig[lastId] = {
+    question: 'question-' + lastId,
+    answer: 'answerlist-' + lastId,
+    options: [{ input: 'optionsquestion-' + 1 + '-' + lastId, value: String.fromCharCode('A'.charCodeAt()) }],
+  };
+
+  let question = document.createElement('div');
+  question.classList.add('question');
+  question.setAttribute('id-question', lastId);
+  question.tabIndex = lastId;
+
+  let optionsHtml = '<div id="optionsquestion'+ lastId +'" class="options-question"><p>Alternativas:</p><div class="options-list"><input class="form-control" type="text"  alternative="A" id="'+questionConfig[lastId].options[0].input + '" name="option'+ lastId +'" placeholder="Alternativa A" required></div><button id="addOption'+ lastId +'" class="btn-default" type="button" onclick="createOption('+ lastId +')">Adicionar Opção</button></div>';
+  let answerHtml = '<div class="answer"><p>Resposta:</p><select id="answerlist-'+ lastId +'" "name="answerlist'+ lastId +'" required><option value="A">A</option></select></div>';
+
+  question.innerHTML = '<label for="question">Pergunta:</label><input id="' + questionConfig[lastId].question + '" class="form-control" type="text" name="question['+ lastId +'][question]" placeholder="Pergunta '+lastId+'" required>'+ optionsHtml + answerHtml +'</div>';
+  document.querySelector("#questionRegisterForm .questions-list-fields").appendChild(question);
+}
+
+function createOption(id) {
+  document.querySelector("#questionRegisterForm #addOption"+id).addEventListener('click', function(e){
+    e.preventDefault();
+    const question = questionConfig[id];
+    const size = question.options.length;
+
+    if (size >= maxOptions) {
+      return;
+    }
+
+    const newOption = { input: 'optionsquestion-' + (question.options.length + 1) + '-' + id, value: String.fromCharCode('A'.charCodeAt() + question.options.length) };
+    question.options.push(newOption);
+
+    let option = document.createElement('input');
+    option.setAttribute('id', newOption.input);
+    option.setAttribute('alternative', newOption.value);
+    option.setAttribute('type', 'text');
+    option.setAttribute('name', newOption.input);
+    option.placeholder = 'Alternativa ' + newOption.value;
+    document.querySelector("#questionRegisterForm .options-list").appendChild(option);
+
+    let answer = document.createElement('option');
+    answer.setAttribute('value', newOption.value);
+    answer.innerText = newOption.value;
+    document.querySelector("#answerlist-"+lastId).appendChild(answer);
+
+    if (question.options.length >= maxOptions) {
+      this.disabled = true;
+      this.classList.add('disabled-button');
+      this.title = 'Você só pode adicionar '+getCookie("maxOptions")+' opções nesta questão';
+    }
+  });
+}
+
+document.querySelector("#questionRegisterForm #sendButton").addEventListener('click', function(e){
+    e.preventDefault();
+
+    submit();
+});
+
+window.onload = function(){
+    document.querySelector("#questionRegisterForm #addQuestion").addEventListener('click', function(e){
+      e.preventDefault();
+      
+      createQuestion();
+      return true;
+    });
+
+    createQuestion();
+
+  // $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+  //     e.preventDefault(); $(this).parent('div').remove(); x--;
+  // })
+};
--- src/routes/test.js
+++ src/routes/test.js
@@ -35,24 +35,25 @@ router.get("/", (req, res) => {
 
 router.post("/store", (req, res) => {
   const tests = JSON.parse(fs.readFileSync('./src/data/test.json', 'utf-8'));
-  const {name, numQuestions} = req.body;
+  const {name, numQuestions, maxOptions} = req.body;
   const id = uuid.v1();
   const slug = slugify(req.body.name);
-  const test = {id, slug, name, numQuestions}; 
+  const test = {id, slug, name, numQuestions, maxOptions}; 
   tests.push(test);
   fs.writeFileSync("./src/data/test.json", JSON.stringify(tests), {encoding: "utf-8"});
 
   res.redirect("http://localhost:3000/test");
+  res.end();
 });
 
 router.get("/:slug/question", (req, res) => {
   let slug = req.params.slug;
-  let tests = JSON.parse(fs.readFileSync('./src/data/test.json', 'utf-8'));
+  let test = JSON.parse(fs.readFileSync('./src/data/test.json', 'utf-8')).filter(test => test.slug === slug);
   let html = fs.readFileSync('././assets/html/index.html', 'utf-8');
   let question = fs.readFileSync('././assets/html/question/question.html', 'utf-8');
-  question = question.replace("{{slug}}", req.params.slug);
+  question = question.replace("{{testId}}", test[0].id);
   html = html.replace("{{component}}", question);
-  let test = tests.filter(test => test.slug === slug);
+  html = html.replace("{{jsCustom}}", '<script src="/js/question.js"></script>')
   
   if(test.length === 1){
     res.cookie('maxQuestions', test[0].numQuestions);
@@ -62,27 +63,39 @@ router.get("/:slug/question", (req, res) => {
   res.end(html);
 });
 
-router.post("/:slug/question/store", async (req, res) => {
-  let slug = req.params.slug;
-  let tests = JSON.parse(fs.readFileSync('./src/data/test.json', 'utf-8'));
+router.post("/question/store", async (req, res) => {
+  let testId = req.body.test_id;
+  let testFile = JSON.parse(fs.readFileSync('./src/data/test.json', 'utf-8'));
   // const {name, email, password} = req.body;
   // const id = uuid.v1();
   // const question = {id, name, email, password}; 
   // users.push(user);
-  
-  let test = tests.map((test) => {
-    if(slug === test.slug){
-      if(test.questions === null || test.questions === "undefined"){
-        test.questions = [];
-      }else{
-        test.questions.push({"oi3": "ola3"},{"pa4": "peeee4444"});
+  let testData = testFile.map((t) => {
+      if(testId === t.id){
+        if(t.questions === null || t.questions === "undefined"){
+          t.questions = [];
+        }else{
+          t.questions.push(req.body.questions);
+        }
       }
-    }
+      
+      return test;
+    });
+  
+    fs.writeFileSync("./src/data/test.json", JSON.stringify(testData), {encoding: "utf-8"});
+  res.status(201).send('oiiii');
+  // let test = tests.map((t) => {
+  //   if(testId === t.id){
+  //     if(t.questions === null || t.questions === "undefined"){
+  //       t.questions = [];
+  //     }else{
+  //     }
+  //   }
     
-    return test;
-  });
+  //   return test;
+  // });
 
-  fs.writeFileSync("./src/data/test.json", JSON.stringify(test), {encoding: "utf-8"});
+  // fs.writeFileSync("./src/data/test.json", JSON.stringify(test), {encoding: "utf-8"});
 
   //dataFile.push(req.body);
   //fs.writeFileSync(jsonPath, JSON.stringify(dataFile));
